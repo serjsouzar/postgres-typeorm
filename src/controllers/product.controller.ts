dd766f76c2ad6fb81b7b567e82349bfdd8294d9c
Sergio Souza <serjsouza93@gmail.com>
2023-12-18 10:18:45 -0300

    getAll and getById

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,18 +1,43 @@
-import { Request, Response } from "express"
-import { Product } from "./../entity/Product"
+import { Request, Response } from "express";
+import { Product } from "./../entity/Product";
 
-export const createProduct = async (req:Request, res:Response) => {
+export const createProduct = async (req: Request, res: Response) => {
   try {
-    const {name, description, value} = req.body
-    const product = new Product()
-    
-    product.name = name
-    product.description = description
-    product.value = value
+    const { name, description, value } = req.body;
+    const product = new Product();
 
-    await product.save()
+    product.name = name;
+    product.description = description;
+    product.value = value;
 
+    await product.save();
+
+    return res.status(201).json(product);
   } catch (error) {
-    console.error(error)
+    if (error instanceof Error)
+      return res.status(500).json({ message: error.message });
   }
-}
+};
+
+export const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find();
+    return res.status(200).json(products);
+  } catch (error) {
+    if (error instanceof Error)
+      return res.send(500).json({ message: error.message });
+  }
+};
+
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const products = await Product.findBy({
+      id: parseInt(id),
+    });
+    return res.status(200).json(products);
+  } catch (error) {
+    if (error instanceof Error)
+      return res.send(500).json({ message: error.message });
+  }
+};
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createProduct } from "./../controllers/product.controller";
+import { createProduct, getAllProducts, getProductById } from "./../controllers/product.controller";
 
 const router = Router()
 
 router.post('/products', createProduct )
+router.get('/products', getAllProducts )
+router.get('/products/:id', getProductById )
 
 export default router
