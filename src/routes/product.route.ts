b6f4efb465b90b0e3738de47edf762590bca1e56
Sergio Souza <serjsouza93@gmail.com>
2023-12-18 13:11:50 -0300

    validations using Joi

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createProduct, deleteProduct, getAllProducts, getProductById, updateProduct } from "./../controllers/product.controller";
+import { validatioMiddleware, productSchema, productUpdateSchema } from "../schemas/product.validation";
 
 const router = Router()
 
-router.post('/products', createProduct )
+router.post('/products', validatioMiddleware(productSchema),createProduct )
 router.get('/products', getAllProducts )
 router.get('/products/:id', getProductById )
-router.put('/products/:id', updateProduct )
+router.put('/products/:id',validatioMiddleware(productUpdateSchema), updateProduct )
 router.delete('/products/:id', deleteProduct )
 
 export default router
---
+++ src/schemas/product.validation.ts
@@ -0,0 +1,33 @@
+import Joi from 'joi'
+import { Request, Response, NextFunction } from 'express'
+
+export const productSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  description: Joi.string().min(5).required(),
+  value: Joi.number().required()
+})
+.options({abortEarly: false})
+
+
+export const productUpdateSchema = Joi.object({
+  name: Joi.string().min(3),
+  description: Joi.string().min(5),
+  value: Joi.number()
+})
+.options({abortEarly: false})
+
+export const validatioMiddleware = (schema:Joi.ObjectSchema) => {
+  return (req:Request, res:Response, next:NextFunction) => {
+    const {error} = schema.validate(req.body);
+
+    if(error) {
+      console.log(error.message)
+      res.status(400).json({ errors: error.details })
+    }
+    else {
+      next();
+    }
+  }
+}
+
+
